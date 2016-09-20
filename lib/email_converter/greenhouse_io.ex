d491701ecb46a6edef86c6cb0f50dd774df01a3d
Brandon Richey <brandon.richey@greenhouse.io>
2016-09-20 11:10:11 -0400

    Removes Mail and replaces it with Mailibex

--- lib/email_converter/greenhouse_io.ex
+++ lib/email_converter/greenhouse_io.ex
@@ -10,12 +10,25 @@ defmodule EmailConverter.GreenhouseIo do
   end
 
   # PRIVATE FUNCTIONS
+  defp greenhouse_url(path) do
+    protocol = Application.get_env(:email_converter, :protocol) || "https"
+    hostname = Application.get_env(:email_converter, :greenhouse_host)
+    port     = Application.get_env(:email_converter, :port)
+    port_string = if port do
+      ":#{port}"
+    else
+      ""
+    end
+    "#{protocol}://#{hostname}#{port_string}/#{path}"
+  end
+
   defp fetch(id, 3, _) do
     {:error, "Tried 3 times to fetch for #{id}. Giving up!"}
   end
 
   defp fetch(id, retries, api_key) do
-    case HTTPoison.get("http://localhost-api.greenhouse.io:3001/v1/candidates/#{id}/activity_feed", headers(api_key)) do
+    IO.puts "Fetching from #{greenhouse_url("v1/candidates/#{id}/activity_feed")}..."
+    case HTTPoison.get(greenhouse_url("v1/candidates/#{id}/activity_feed"), headers(api_key)) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         {:ok, body}
       {:ok, %HTTPoison.Response{status_code: 429}} ->
